a1f32fbd0921ecebc89ca6961b028284807d65b8
gopikanair22 <gopikanair22@gmail.com>
2017-04-07 11:22:27 -0400

    package name change

--- order-service/src/main/java/com/cloudnativecoffee/order/exception/OrderDeletionException.java
+++ order-service/src/main/java/com/cloudnativecoffee/order/exception/OrderDeletionException.java
@@ -1,4 +1,4 @@
-package com.cloudnativecoffee.exception;
+package com.cloudnativecoffee.order.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
